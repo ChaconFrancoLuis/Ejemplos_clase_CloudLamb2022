1999816d8608c578bdc3f346ec91481b3e043566
ChaconFrancoLuis <francofer7775@gmail.com>
2022-03-26 18:40:30 +0000

    agregando ejemplos API y challenge de API

---
+++ api examples/caracteresraw.py
@@ -0,0 +1,7 @@
+
+a = 'hello\nworld' # el \n backslash da saltos de linea.
+b = r'hello\nmundo' #una cadena de caracteres r ayuda a mandar la informacion raw (completa).
+
+print(a)
+print('\n')
+print(b)
---
+++ api examples/ejemplosAPI.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+#def chuck(): #definicion de una funcion llamada chuck
+#    f = r"http://api.icndb.com/jokes/random" #Raw string (para evitar cualquier salto de linea)
+#    data = requests.get(f) #get request, read
+    #print(type(data)) 
+#    tt = json.loads(data.text) #parsing json a python 
+#    print(tt["value"]["joke"])
+    #print(type(tt))  #dictionary
+
+#chuck()
+#-------------------------------------Challenge!---------------------------------------
+# crea una API que te muestre una lista de categorias de la api en : http://www.icndb.com/api/
+# Luego crea una forma en la cual seleccionar de forma continua un chiste de cualquier categoria
+# incluye la opcion quit o q para finalizar la ejecucion del programa.
+
+
+
+
+#--------------------------------------------------------------------------------------------
+##############################SWAPI! una API de star wards##################################
+#import pprint
+#def swapi():
+#    ask = input("escoge una categoria: Planets, Spaceships, Vehicles, People, Films and Species: ")
+#    f = 'https://swapi.dev/api/{}'.format(ask)
+#    data = requests.get(f)
+#    tt = json.loads(data.text)
+#    print(type(tt))
+#    pprint.pprint(tt)
+
+
+#swapi()
+#-------------------------------------Challenge!---------------------------------------
+# crea una API que te muestre una lista de categorias de la api en : https://swapi.dev/documentation
+# Luego crea una forma en la cual seleccionar de forma continua una categoria
+# incluye la opcion quit o q para finalizar la ejecucion del programa.
