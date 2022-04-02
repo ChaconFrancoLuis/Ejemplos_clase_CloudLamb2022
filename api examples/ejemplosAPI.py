ff7cfa730dba9ee1d64252cf09b867a53f0046af
chaconfranco7775 <77419204+chaconfranco7775@users.noreply.github.com>
2022-04-02 13:14:34 +0000

    cambios realizados (#2)

--- api examples/ejemplosAPI.py
+++ api examples/ejemplosAPI.py
@@ -1,15 +1,15 @@
 import requests
 import json
 
-#def chuck(): #definicion de una funcion llamada chuck
-#    f = r"http://api.icndb.com/jokes/random" #Raw string (para evitar cualquier salto de linea)
-#    data = requests.get(f) #get request, read
+def chuck(): #definicion de una funcion llamada chuck
+    f = r"http://api.icndb.com/jokes/random" #Raw string (para evitar cualquier salto de linea)
+    data = requests.get(f) #get request, read
     #print(type(data)) 
-#    tt = json.loads(data.text) #parsing json a python 
-#    print(tt["value"]["joke"])
-    #print(type(tt))  #dictionary
+    tt = json.loads(data.text) #parsing json a python 
+    print(tt["value"]["joke"])
+    print(type(tt))  #dictionary
 
-#chuck()
+chuck()
 #-------------------------------------Challenge!---------------------------------------
 # crea una API que te muestre una lista de categorias de la api en : http://www.icndb.com/api/
 # Luego crea una forma en la cual seleccionar de forma continua un chiste de cualquier categoria
